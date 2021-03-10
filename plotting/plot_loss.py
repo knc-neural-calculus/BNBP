da1bd747ce79caba87572a53d9cdac29995194b1
mivanit <mivanits@umich.edu>
2021-03-10 15:05:40 -0500

    updating loss plotting script with documentation

--- plotting/plot_loss.py
+++ plotting/plot_loss.py
@@ -1,41 +1,86 @@
 import os
 import sys
+import warnings
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+class LossPlotters(object):
 
-def plot_loss(dirname):
+	@staticmethod
+	def plot_loss(dirname : str, verbose : bool = True, show : bool = True):
+		"""### plot_loss
+		
+		plot the loss for a single run. if a file is not found, function will print a warning and do nothing
+				
+		### Parameters:
+		 - `dirname : str`   
+		   directory to look for `loss.txt` in
+		 - `verbose : bool`   
+		   whether to print the average loss
+		   (defaults to `True`)
+		 - `show : bool`   
+		   whether to show the plot upon completion (set to False if wrapped in another script)
+		   (defaults to `True`)
+		"""
 
-	filename = dirname + 'loss.txt'
-	
-	if not os.path.isfile(filename):
-		return
+		filename = dirname + 'loss.txt'
+		
+		# do nothing on error
+		if not os.path.isfile(filename):
+			warnings.warn(f'invalid directory, could not find\t{filename}')
+			return
 
-	data = np.genfromtxt(filename, delimiter=',')
-	data = data[:,:-1]
+		data = np.genfromtxt(filename, delimiter=',')
+		data = data[:,:-1]
 
-	data_avg = np.average(data, axis = 1)
+		data_avg = np.average(data, axis = 1)
+		
+		if verbose:
+			print(data_avg)
 
-	print(data_avg)
+		plt.plot(np.arange(0,len(data_avg),1), data_avg, label = dirname)	
 
-	plt.plot(np.arange(0,len(data_avg),1), data_avg, label=dirname)	
+		if show:
+			plt.show()
+			
+
+	@staticmethod
+	def plot_loss_all_in_dir(rootdir : str = '../../data/', verbose : bool = True):
+		"""### plot_loss_all_in_dir
+		
+		given a directory full of run data, plot the loss for each of those runs
+				
+		### Parameters:
+		 - `rootdir : str`   
+		   searches every directory in this directory for loss files
+		   (defaults to `'../../data/'`)
+		 - `verbose : bool`   
+		   whether to print info about which directory is being searched
+		   (defaults to `True`)
+		"""
+
+		dirnames = os.listdir(rootdir)
+
+		if verbose:
+			print('plotting data:')
+		
+		for d in dirnames:
+			print('\t' + d)
+			LossPlotters.plot_loss(d, verbose = verbose, show = False)
+
+		plt.legend()
+		plt.show()
+
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		dirnames = sys.argv[1:]
-	else:
-		dirnames = os.listdir('../../data/')
-		dirnames = [ '../../data/' + x + '/' for x in dirnames ]
+	import fire
+	fire.Fire(LossPlotters)
 
 
-	print('plotting data:')
-	for d in dirnames:
-		print('\t' + d)
-		plot_loss(d)
 
-	plt.legend()
-	plt.show()
+
 	
 
 
